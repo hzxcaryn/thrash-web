098d6fede01f9a2a5b86ab1090fc7447b2e3fd28
hzxcaryn <heng.zhixuan.caryn@dhs.sg>
2018-02-25 05:55:56 +0800

    Clean up js

--- js/admin.js
+++ js/admin.js
@@ -488,45 +488,6 @@ var generalChart = new Chart(ctx, {
 });
 */
 
-Chart.defaults.global.defaultFontColor = '#9c9fce';
-$(function () {
-    new Chart(document.getElementById("generalChart").getContext("2d"), getChartJs('line'));
-});
-
-function getChartJs(type) {
-    var config = null;
-
-    if (type === 'line') {
-        config = {
-            type: 'line',
-            data: {
-                labels: ["January", "February", "March", "April", "May", "June", "July"],
-                datasets: [{
-                    label: "My First dataset",
-                    data: [65, 59, 80, 81, 56, 55, 40],
-                    borderColor: 'rgba(0, 188, 212, 0.75)',
-                    backgroundColor: 'rgba(0, 188, 212, 0.3)',
-                    pointBorderColor: 'rgba(0, 188, 212, 0)',
-                    pointBackgroundColor: 'rgba(0, 188, 212, 0.9)',
-                    pointBorderWidth: 1
-                }, {
-                        label: "My Second dataset",
-                        data: [28, 48, 40, 19, 86, 27, 90],
-                        borderColor: 'rgba(233, 30, 99, 0.75)',
-                        backgroundColor: 'rgba(233, 30, 99, 0.3)',
-                        pointBorderColor: 'rgba(233, 30, 99, 0)',
-                        pointBackgroundColor: 'rgba(233, 30, 99, 0.9)',
-                        pointBorderWidth: 1
-                    }]
-            },
-            options: {
-                responsive: true,
-                legend: false
-            }
-        }
-    }
-    return config;
-}
 //==========================================================================================================================
 
 $(function () {
--- js/pages/charts/chartjs.js
+++ js/pages/charts/chartjs.js
@@ -1,8 +1,6 @@
+Chart.defaults.global.defaultFontColor = '#9c9fce';
 $(function () {
-    new Chart(document.getElementById("line_chart").getContext("2d"), getChartJs('line'));
-    new Chart(document.getElementById("bar_chart").getContext("2d"), getChartJs('bar'));
-    new Chart(document.getElementById("radar_chart").getContext("2d"), getChartJs('radar'));
-    new Chart(document.getElementById("pie_chart").getContext("2d"), getChartJs('pie'));
+    new Chart(document.getElementById("generalChart").getContext("2d"), getChartJs('line'));
 });
 
 function getChartJs(type) {
@@ -14,98 +12,22 @@ function getChartJs(type) {
             data: {
                 labels: ["January", "February", "March", "April", "May", "June", "July"],
                 datasets: [{
-                    label: "My First dataset",
+                    label: "Food Waste",
                     data: [65, 59, 80, 81, 56, 55, 40],
-                    borderColor: 'rgba(0, 188, 212, 0.75)',
-                    backgroundColor: 'rgba(0, 188, 212, 0.3)',
-                    pointBorderColor: 'rgba(0, 188, 212, 0)',
-                    pointBackgroundColor: 'rgba(0, 188, 212, 0.9)',
+                    borderColor: 'rgba(105, 154, 253, 0.6)',
+                    backgroundColor: 'rgba(90, 84, 149, 0)',
+                    pointBorderColor: 'rgba(105, 154, 253, 0.5)',
+                    pointBackgroundColor: 'rgba(105, 154, 253, 0.9)',
                     pointBorderWidth: 1
                 }, {
-                        label: "My Second dataset",
+                        label: "Others",
                         data: [28, 48, 40, 19, 86, 27, 90],
-                        borderColor: 'rgba(233, 30, 99, 0.75)',
-                        backgroundColor: 'rgba(233, 30, 99, 0.3)',
-                        pointBorderColor: 'rgba(233, 30, 99, 0)',
-                        pointBackgroundColor: 'rgba(233, 30, 99, 0.9)',
+                        borderColor: 'rgba(249, 106, 149, 0.6)',
+                        backgroundColor: 'rgba(90, 84, 149, 0)',
+                        pointBorderColor: 'rgba(249, 106, 149, 0.5)',
+                        pointBackgroundColor: 'rgba(249, 106, 149, 0.9)',
                         pointBorderWidth: 1
                     }]
-            },
-            options: {
-                responsive: true,
-                legend: false
-            }
-        }
-    }
-    else if (type === 'bar') {
-        config = {
-            type: 'bar',
-            data: {
-                labels: ["January", "February", "March", "April", "May", "June", "July"],
-                datasets: [{
-                    label: "My First dataset",
-                    data: [65, 59, 80, 81, 56, 55, 40],
-                    backgroundColor: 'rgba(0, 188, 212, 0.8)'
-                }, {
-                        label: "My Second dataset",
-                        data: [28, 48, 40, 19, 86, 27, 90],
-                        backgroundColor: 'rgba(233, 30, 99, 0.8)'
-                    }]
-            },
-            options: {
-                responsive: true,
-                legend: false
-            }
-        }
-    }
-    else if (type === 'radar') {
-        config = {
-            type: 'radar',
-            data: {
-                labels: ["January", "February", "March", "April", "May", "June", "July"],
-                datasets: [{
-                    label: "My First dataset",
-                    data: [65, 25, 90, 81, 56, 55, 40],
-                    borderColor: 'rgba(0, 188, 212, 0.8)',
-                    backgroundColor: 'rgba(0, 188, 212, 0.5)',
-                    pointBorderColor: 'rgba(0, 188, 212, 0)',
-                    pointBackgroundColor: 'rgba(0, 188, 212, 0.8)',
-                    pointBorderWidth: 1
-                }, {
-                        label: "My Second dataset",
-                        data: [72, 48, 40, 19, 96, 27, 100],
-                        borderColor: 'rgba(233, 30, 99, 0.8)',
-                        backgroundColor: 'rgba(233, 30, 99, 0.5)',
-                        pointBorderColor: 'rgba(233, 30, 99, 0)',
-                        pointBackgroundColor: 'rgba(233, 30, 99, 0.8)',
-                        pointBorderWidth: 1
-                    }]
-            },
-            options: {
-                responsive: true,
-                legend: false
-            }
-        }
-    }
-    else if (type === 'pie') {
-        config = {
-            type: 'pie',
-            data: {
-                datasets: [{
-                    data: [225, 50, 100, 40],
-                    backgroundColor: [
-                        "rgb(233, 30, 99)",
-                        "rgb(255, 193, 7)",
-                        "rgb(0, 188, 212)",
-                        "rgb(139, 195, 74)"
-                    ],
-                }],
-                labels: [
-                    "Pink",
-                    "Amber",
-                    "Cyan",
-                    "Light Green"
-                ]
             },
             options: {
                 responsive: true,
